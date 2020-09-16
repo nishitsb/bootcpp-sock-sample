06fa51b5de160e762a7305bcce660c2730200022
nishitsb <nishitsbansal1997@gmail.com>
2020-09-16 19:48:03 +0530

    implemented without serialiazation

--- server.cpp
+++ server.cpp
@@ -1,56 +1,69 @@
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 #define PORT 8080
 
-int main(int argc, char const *argv[]) 
-{ 
+int main(int argc, char const *argv[])
+{
 	int server_fd = 0;
-    int new_socket = 0; 
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	char buffer[1024] = {0}; 
-	const char *hello = "Hello from server"; 
+	int new_socket = 0;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = {0};
+	const char *hello = "Hello from server";
 	
-	// Creating socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-	{ 
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	} 
+	struct Certificate
+	{
+		char name[38];
+		unsigned int hash;
+	};
+
+	Certificate c1 = {"NishitSBansal", 123456};
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Attach socket to the port 8080
+	if (bind(server_fd, (struct sockaddr *)&address,
+			 sizeof(address)) < 0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	// Setup listen queue
+	if (listen(server_fd, 3) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
 	
-	address.sin_family = AF_INET; 
-	address.sin_addr.s_addr = INADDR_ANY; 
-	address.sin_port = htons( PORT ); 
+	printf("Waiting for connection...\n");
 	
-	// Attach socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
-	{ 
-		perror("bind failed"); 
-		exit(EXIT_FAILURE); 
-	} 
-    // Setup listen queue
-	if (listen(server_fd, 3) < 0) 
-	{ 
-		perror("listen"); 
-		exit(EXIT_FAILURE); 
-	} 
- 	printf("Waiting for connection...\n");
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0) 
-	{ 
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	} 
-  	printf("Waiting for data...\n");
-	read( new_socket , buffer, 1024); 
-	printf("Received: %s\n",buffer ); 
-	send(new_socket , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
-	return 0; 
-} 
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+							 (socklen_t *)&addrlen)) < 0)
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Sending certificate...");
+	send(new_socket, &c1, sizeof(c1), 0);
+	read(new_socket, buffer, 1024);
+	printf("Received: %s\n", buffer);
+	send(new_socket, hello, strlen(hello), 0);
+	printf("Hello message sent\n");
+	
+	return 0;
+}
